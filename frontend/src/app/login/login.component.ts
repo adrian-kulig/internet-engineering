3fe3644dbde1cead81812d6ec32ac6d99a38e229
dkrol <damiankrolll@gmail.com>
2018-12-29 19:24:10 +0100

    Login logic moved from app.component to login.component

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { User } from '../models/user';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent{
 
-  constructor() { }
+  user: User = new User();
 
-  ngOnInit() {
+  constructor(private authService: AuthService,) {
+    this.authService.loggedIn.subscribe(loggedIn => {
+      this.user.loggedIn = loggedIn;
+    });
   }
 
+  doLogin() {
+    this.authService.doLogin(this.user.email, this.user.password);
+  }
+
+  doLogout() {
+    this.authService.logout();
+  }
+
+  onSubmit() {
+    this.doLogin();
+  }
 }
