812f6e076454b8d86e4768ab6c539d608456efbb
dkrol <damiankrolll@gmail.com>
2018-12-29 13:48:05 +0100

    Login component added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,12 +11,14 @@ import {ToastrModule} from 'ngx-toastr';
 import {FormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OfferComponent,
     HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
