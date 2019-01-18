eeabdb216bdaced9cb9b1987e15427032e18c0bc
Adrian Kulig <adrian.kulig@quantoo.pl>
2019-01-18 13:18:36 +0100

    get single Comment by id

--- frontend/src/app/comment/comment-edit/comment-edit.component.ts
+++ frontend/src/app/comment/comment-edit/comment-edit.component.ts
@@ -4,6 +4,7 @@ import { CommentService } from '../../services/comment/comment.service';
 import { CommentHelperService } from '../../utils/comment-helper.service';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import {AuthService} from "../../services/auth/auth.service";
 
 @Component({
   selector: 'app-comment-edit',
@@ -12,24 +13,53 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class CommentEditComponent implements OnInit {
 
-  comment: Comments;
+  comment: Comments = null;
+  id: string;
 
   constructor(private commentService: CommentService,
               private commentServiceHelper: CommentHelperService,
               private route: ActivatedRoute,
-              private toastr: ToastrService) {
-    }
-
-  ngOnInit() {
-    this.commentService.getCommentById
+              private toastr: ToastrService
+  ) {
+    this.route.params.subscribe(params => {
+      this.id = params.id;
+    });
   }
 
-  editCommentAction(commentId, comment: Comments) {
-    this.commentService.editComment(commentId, comment).subscribe( data => {
+
+  ngOnInit() {
+
+    if (!AuthService.SessionStorageManager.getValue('user')) {
+      this.toastr.error('musisz byc zalogowany');
+      // this.offerServiceHelper.redirect('/','error','Musisz być zalogowany');
+      return false;
+    }
+    this.commentService.getCommentById(this.id).subscribe(
+      data => {
+        if (data.user && data.user.id == AuthService.getLoggedInUser().id) {
+          console.log(data);
+          this.comment = data
+        } else {
+          this.toastr.error('Nie mozesz edytowac tego komentarza');
+          // this.offerServiceHelper.redirect('/offers', 'error', 'Nie możesz edytować tej oferty.');
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  editCommentAction(comment: Comments) {
+    this.commentService.editComment(this.id, comment).subscribe( data => {
         this.toastr.success("Komentarz został zapisany poprawnie");
     },
     (errorResp) => {
         this.toastr.error(errorResp.error.errorMessage ? errorResp.error.errorMessage : 'Coś poszło nie tak.')
     });
+  }
+
+  onSubmit(){
+    this.editCommentAction(this.comment);
   }
 }
